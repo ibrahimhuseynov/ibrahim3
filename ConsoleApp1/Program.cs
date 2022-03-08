bcae6702a6a60cb831f87223a42c9eab4e416990
ibrahimhuseynov <tu7pdt8l8@code.edu.az>
2022-03-08 11:02:12 +0400

    new projekt twe

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+       
+            static void Main(string[] args)
+            {
+                int[] arry = { 12, 13, 12, 234, 435, 35, 34, 34, 345, 6565, 25 };
+                Console.WriteLine(number(arry));
+                Console.ReadLine();
+            }
+            static int number(int[] arry)
+            {
+                int sum = 0;
+                for (int i = 0; i < arry.Length; i++)
+                {
+                    sum = sum + arry[i];
+                }
+                return sum;
+            }
+        
+    }
+}
